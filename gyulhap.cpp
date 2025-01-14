02a4493118224ea0b87cd60f0ab0cb00de7fcf6b
Ryan Ho <ryanhohoho6@gmail.com>
2025-01-14 18:06:20 -0500

    made the game board class and the majority of its functions, just the findsolutions left

--- gyulhap.cpp
+++ gyulhap.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <vector>
-using namespace std;
 
-class Board {
-    private:
-        vector<Card> cards;
-    public:
-        vector<Card> getCards();
-        vector<Card> generateCards();
-        vector<Card> displayCards();
-        vector<Card> drawCards();
-        Board();
-};
+#include <bits/stdc++.h>
+using namespace std;
 
 class Card {
     private:
@@ -21,8 +14,81 @@ class Card {
         Card(vector<int> array);
 
         vector<int> getAttributes();
+        void printAttributes();
         Card findThirdCard(Card otherCard);
 };
+
+class Board {
+    private:
+        vector<Card> cards;
+        vector<string> solutions;
+    public:
+        vector<Card> getCards();
+        vector<string> getSolutions();
+        void generateCards(int amount, int traits);
+        void findSolutions();
+        void displayCards(int length, int width);
+        // vector<Card> drawCards();
+        Board(int amount, int traits);
+};
+
+Board::Board(int amount, int traits) {
+    this->generateCards(amount, traits);
+    this->findSolutions();
+}
+
+vector<Card> Board::getCards() {
+    return this->cards;
+}
+
+vector<string> Board::getSolutions() {
+    return this->solutions;
+}
+
+void Board::generateCards(int amount, int traits) {
+    vector<int> values;
+
+    for (int i = 0; i < amount; i++) {
+        for (int i = 0; i < traits; i++) {
+            int randomNum = rand() % 3;
+            values.push_back(randomNum);
+        }
+
+        Card newCard(values);
+        (this->cards).push_back(newCard);
+
+        values.clear();
+    }
+}
+
+void Board::findSolutions() {
+    vector<Card> visited;
+    vector<int> visitedIndex;
+
+    // for (int i = 0; i < this->getCards().size(); i++) {
+    //     for (int j = i; j < this->getCards().size(); j++) {
+    //         Card search = (this->getCards()).at(i).findThirdCard(this->getCards().at(j));
+
+    //         auto it = find(visited.begin(), visited.end(), search);
+    //         if (it != visited.end()) {
+
+    //         }
+    //     }
+
+    //     // I have to define the push back function for Card vectors
+    //     // visited.push_back();
+    // }
+}
+
+void Board::displayCards(int length, int width) {
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < width; j++) {
+            (cards.at(i*length + j)).printAttributes();
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
 
 Card::Card(vector<int> array) {
     this->attributes = array;
@@ -32,11 +98,19 @@ vector<int> Card::getAttributes() {
     return this->attributes;
 }
 
+void Card::printAttributes() {
+    cout << "(";
+    for (int i = 0; i < this->getAttributes().size(); i++) {
+        cout << this->getAttributes().at(i);
+    }
+    cout << ")";
+}
+
 Card Card::findThirdCard(Card otherCard) {
     int value;
     vector<int> newAttributes;
 
-    for (int i = 0; i < this->attributes.size(); i++) {
+    for (int i = 0; i < this->getAttributes().size(); i++) {
         value = ((this->getAttributes().at(i) * -1) + (otherCard.getAttributes().at(i) * -1) + 6) % 3;
         newAttributes.push_back(value);
     }
@@ -45,10 +119,18 @@ Card Card::findThirdCard(Card otherCard) {
 }
 
 int main() {
-    Card card1({0, 1, 1});
-    Card card2({2, 2, 2});
+    srand(time(0));
+    Card card1({0, 1, 0});
+    Card card2({2, 1, 0});
 
-    Card result = card1.findThirdCard(card2);
+    // card1.printAttributes();
+    // card2.printAttributes();
+
+    // Card result = card1.findThirdCard(card2);
+    // result.printAttributes();
+
+    Board gameBoard(9, 3);
+    gameBoard.displayCards(3, 3);
 
     return 0;
 }
