11bfd0912b1bd8ec83ffc0480864a7703b000342
Ryan Ho <ryanhohoho6@gmail.com>
2025-03-18 22:50:52 -0400

    experimenting with pygame

--- gyulhap.py
+++ gyulhap.py
@@ -1,3 +1,6 @@
+import pygame
+import sys
+
 class Cell:
     def __init__(self, color, shape, background):
         self.color = color
@@ -10,6 +13,25 @@ class Cell:
 def main():
     p1 = Cell("Red", "Square", "Gray")
     print(p1)
+
+    pygame.init()
+    screenWidth = 400
+    screenHeight = 300
+
+    screen = pygame.display.set_mode((screenWidth, screenHeight))
+    pygame.display.set_caption("Gyul Hap")
+
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.fill((255, 255, 255))
+        pygame.display.flip()
+        clock.tick(40)
 
 if __name__=="__main__":
     main()
