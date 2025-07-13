25af217ed403241a75e40102843ed0bd3fcc467b
Ryan Ho <ryanhohoho6@gmail.com>
2025-07-13 11:02:35 -0400

    finally have internet back, been too long, going to reset

--- gyulhap.py
+++ gyulhap.py
@@ -1,37 +1,5 @@
-import pygame
-import sys
-
-class Cell:
-    def __init__(self, color, shape, background):
-        self.color = color
-        self.shape = shape
-        self.background = background
-
-    def __str__(self):
-        return f"{self.color}::{self.shape}::{self.background}"
-
 def main():
-    p1 = Cell("Red", "Square", "Gray")
-    print(p1)
-
-    pygame.init()
-    screenWidth = 400
-    screenHeight = 300
-
-    screen = pygame.display.set_mode((screenWidth, screenHeight))
-    pygame.display.set_caption("Gyul Hap")
-
-    clock = pygame.time.Clock()
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-        screen.fill((255, 255, 255))
-        pygame.display.flip()
-        clock.tick(40)
+    print("Hello World")
 
 if __name__=="__main__":
     main()
