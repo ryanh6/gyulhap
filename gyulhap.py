6cadcf91cc77bbb4d4e15b025441912d4e7d646a
Ryan Ho <ryanhohoho6@gmail.com>
2025-07-17 23:43:44 -0400

    done basic cell, board and game class

--- gyulhap.py
+++ gyulhap.py
@@ -4,8 +4,48 @@ class Player:
         self.name = name
         self.points = 0
 
+    def addPoints(self, points):
+        self.points += points
+
     def __str__(self):
-        return f'Player {self.id}: {self.name}'
+        return f"Player {self.id}: {self.name}"
+
+class Cell:
+    def __init__(self, colour, shape, background):
+        self.colour = colour
+        self.shape = shape
+        self.background = background
+
+    def __str__(self):
+        return f"{self.colour}, {self.shape}, {self.background}"
+
+class Board:
+    def __init__(self, size):
+        self.size = size
+        self.grid = []
+        
+        for i in range(size):
+            row = []
+            for j in range(size):
+                row.append(Cell(None, None, None))
+            
+            self.grid.append(row)
+
+    def getCell(self, x, y):
+        return self.grid[x][y]
+
+    def setCell(self, x, y, colour, shape, background):
+        self.grid[x][y] = Cell(colour, shape, background)
+
+    def __str__(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                print(self.grid[i][j])
+
+class Game:
+    def __init__(self, size):
+        self.board = Board(size)
+        self.players = [Player(1, "Alice"), Player(2, "Bob")]
 
 def main():
     p1 = Player(1, "Alice")
