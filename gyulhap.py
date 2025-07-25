3ba93fe7bd6882e2d960eae64a8b3578ef881f8b
Ryan Ho <ryanhohoho6@gmail.com>
2025-07-24 23:04:44 -0400

    removed 3d grid in place of array, removed grid and tuple as redundant

--- gyulhap.py
+++ gyulhap.py
@@ -17,23 +17,26 @@ class Cell:
     def __init__(self, attributes):
         self.attributes = attributes
 
+    def __eq__(self, other):
+        if (self.attributes == other.attributes):
+            return True
+        return False
+
     def getAttributes(self):
         return self.attributes
-
+    
     def thirdCell(self, other):
-        properties = []
+        values = []
 
         for i in range(len(self.attributes)):
             sum = self.attributes[i] + other.attributes[i]
             rounded = 3 * math.ceil(sum / 3)
 
             desired = rounded - sum
-            properties.append(desired)
+            values.append(desired)
 
-        desiredProperties = tuple(properties)
-        
-        print(desiredProperties)
-        return desiredProperties
+        desiredCell = Cell(tuple(values))
+        return desiredCell
 
     def __str__(self):
         return f"{self.attributes}"
@@ -42,68 +45,54 @@ class Board:
     def __init__(self, length, width, attributeNum):
         self.length = length
         self.width = width
+        self.size = length * width
         self.attributeNum = attributeNum
         self.grid = []
-        self.cellSet = []
-        
-        for i in range(length):
-            row = []
-            for j in range(width):
-                row.append(Cell((0,) * attributeNum))
-            
-            self.grid.append(row)
+
+        for i in range(self.size):
+            self.grid.append(Cell((0,) * attributeNum))
 
         self.generateBoard()
-        # self.findSolutions()
 
-    def getCell(self, x, y):
-        return self.grid[x][y]
+    def getCell(self, index):
+        return self.grid[index]
 
-    def setCell(self, x, y, attributes):
-        self.grid[x][y] = Cell(attributes)
+    def setCell(self, index, newCell):
+        self.grid[index] = newCell
 
     def generateBoard(self):
-        for i in range(self.length):
-            for j in range(self.width):
-                newTuple = tuple(random.randint(0, 2) for i in range(self.attributeNum))
+        for i in range(self.size):
+            newCell = Cell(tuple(random.randint(0, 2) for i in range(self.attributeNum)))
                 
-                while (newTuple in self.cellSet):
-                    newTuple = tuple(random.randint(0, 2) for i in range(self.attributeNum))
+            while (newCell in self.grid):
+                newCell = Cell(tuple(random.randint(0, 2) for i in range(self.attributeNum)))
                 
-                self.setCell(i, j, newTuple)
-                self.cellSet.append(newTuple)
-
-        print(self.cellSet)
-
+            self.setCell(i, newCell)
 
     def __str__(self):
         boardString = ""
+
         for i in range(self.length):
             for j in range(self.width):
-                boardString += str(self.grid[i][j]) + " "
+                boardString += str(self.grid[(i * self.width) + j]) + " "
             boardString += "\n"
         
         return boardString[:-1]
 
 class Game:
     def __init__(self, length, width, attributeNum):
-        # DO SOME SORT OF LENGTH / WIDTH CHECK
-        # LENGTH / WIDTH SHOULDNT EXCEED NUMBER OF PROPERTIES IN A SENSE (?)
         self.board = Board(length, width, attributeNum)
         self.players = [Player(1, "Alice"), Player(2, "Bob")]
 
 def main():
     newGame = Game(3, 3, 3)
     print(newGame.board)
-    cell1 = newGame.board.getCell(0, 0)
-    cell2 = newGame.board.getCell(1, 1)
+    cell1 = newGame.board.getCell(0)
+    cell2 = newGame.board.getCell(1)
     print(cell1)
     print(cell2)
 
-    print(newGame.board.cellSet.index(cell1.getAttributes()))
-    print(newGame.board.cellSet.index(cell2.getAttributes()))
-
-    cell1.thirdCell(cell2)
+    print(cell1.thirdCell(cell2))
 
 if __name__=="__main__":
     main()
