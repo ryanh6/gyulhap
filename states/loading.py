41b4583d71ab5ace56f8891c2f081f38e25255e4
Ryan Ho <ryanhohoho6@gmail.com>
2025-08-29 01:00:53 -0400

    creating the game without timers might help me understand better

--- states/loading.py
+++ states/loading.py
@@ -1,6 +1,6 @@
 from states.state import State
 from states.gaming import Gaming
-from engine.timer import Timer
+# from engine.timer import Timer
 
 import pygame
 
@@ -9,23 +9,23 @@ class Loading(State):
         State.__init__(self, game)
         self.roundNumber = roundNumber
         self.engine = engine
-        self.timer = Timer()
-        self.timer.startTimer(3)
+        # self.timer = Timer()
+        # self.timer.startTimer(3)
 
     def update(self, controls, position):
-        self.timer.update()
+        # self.timer.update()
 
-        if not (self.timer.active):
-            self.gameScreen = Gaming(self.game, self.engine)
-            self.exitState()
-            self.gameScreen.enterState()
+        # if not (self.timer.active):
+        #     self.gameScreen = Gaming(self.game, self.engine)
+        #     self.exitState()
+        #     self.gameScreen.enterState()
 
         self.game.resetKeys()
 
     def draw(self, display, position):
         display.fill((220, 30, 40))
 
-        loadingBarWidth = self.timer.calculatePercentage() * display.get_width()
-        pygame.draw.rect(display, (170, 30, 40), pygame.Rect(0, display.get_height() - 10, loadingBarWidth, 10))
+        # loadingBarWidth = self.timer.calculatePercentage() * display.get_width()
+        # pygame.draw.rect(display, (170, 30, 40), pygame.Rect(0, display.get_height() - 10, loadingBarWidth, 10))
 
 
