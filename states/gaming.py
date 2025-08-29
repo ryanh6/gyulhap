7ff33fad6f3e870e89b83f80dd366ed7b4466b83
Ryan Ho <ryanhohoho6@gmail.com>
2025-08-29 01:06:15 -0400

    removing lines

--- states/gaming.py
+++ states/gaming.py
@@ -1,32 +1,13 @@
 from states.state import State
-from engine.board import Board
-from engine.player import Player
-# from engine.timer import Timer
-
-import pygame
 
 class Gaming(State):
     def __init__(self, game, gameEngine):
         State.__init__(self, game)
-        # self.timer = Timer()
         self.engine = gameEngine
-        # self.timer.startTimer(10)
-
-        self.engine.makeBoard()
 
     def update(self, display, position):
-        # self.timer.update()
-
-        # if not (self.timer.active):
-        #     # self.scoreScreen = Score(self.game, self.engine, 1)
-        #     self.exitState()
-        #     # self.scoreScreen.enterState()
-
         self.game.resetKeys()
 
     def draw(self, display, position):
         display.fill((255, 255, 255))
         self.engine.board.draw(display)
-
-        # loadingBarWidth = self.timer.calculatePercentage() * display.get_width()
-        # pygame.draw.rect(display, (170, 30, 40), pygame.Rect(0, display.get_height() - 10, loadingBarWidth, 10))
--- states/loading.py
+++ states/loading.py
@@ -1,25 +1,12 @@
 from states.state import State
-from states.gaming import Gaming
-# from engine.timer import Timer
-
-import pygame
 
 class Loading(State):
     def __init__(self, game, engine, roundNumber):
         State.__init__(self, game)
         self.roundNumber = roundNumber
         self.engine = engine
-        # self.timer = Timer()
-        # self.timer.startTimer(3)
 
     def update(self, controls, position):
-        # self.timer.update()
-
-        # if not (self.timer.active):
-        #     self.gameScreen = Gaming(self.game, self.engine)
-        #     self.exitState()
-        #     self.gameScreen.enterState()
-
         self.game.resetKeys()
 
     def draw(self, display, position):
