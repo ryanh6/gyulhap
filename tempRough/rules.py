c20c04d4b1444425f795a0e1816275569cb1f340
Ryan Ho <ryanhohoho6@gmail.com>
2025-09-24 16:54:11 -0400

    forgot to add this

--- tempRough/rules.py
+++ tempRough/rules.py
@@ -5,19 +5,19 @@ class Rules(State):
     def __init__(self, game):
         State.__init__(self, game)
 
-        self.exitRulesButton = Button()
+        # self.exitRulesButton = Button()
 
     def update(self, controls, position):
         if (controls["escape"] == True):
             self.exitState()
 
-        if (controls["clicked"] == True):
-            if (self.exitRulesButton.rect.collidepoint(position)):
-                self.exitState()
+        # if (controls["clicked"] == True):
+        #     if (self.exitRulesButton.rect.collidepoint(position)):
+        #         self.exitState()
 
         self.game.resetKeys()
 
     def draw(self, display, position):
         display.fill((255, 255, 255))
 
-        self.exitRulesButton.draw(display, position)
+        # self.exitRulesButton.draw(display, position)
